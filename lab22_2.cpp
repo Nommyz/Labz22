312a580bf99c6e48b3da1402c28a173e5816a639
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-03-12 17:34:40 +0700

    Mission completed

--- lab22_2.cpp
+++ lab22_2.cpp
@@ -22,8 +22,24 @@ int main(){
 	
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	loc = line_up.begin();
+	for(int i=0;i<2;i++) loc++;
+	line_up.erase(line_up.begin(),loc);
+	loc = line_up.begin();
+	for(int i=0;i<2;i++) loc++;
+	line_up.insert(loc,"Narutu");
+	line_up.push_front("Prayath");
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Tony");
+	loc = line_up.begin();
+	for(int j=0;j<4;j++) loc++;
+	line_up.erase(line_up.begin(),loc);
+
 	
-	//Write your code here
+
 	
 	printList(line_up);
 		
